87ffd49cc03021d9e51ef24b25c6a568572fc0ef
Naji Abed <nageaped31@gmail.com>
2025-06-29 19:46:36 +0300

    Finishing courses page

--- script/courses.js
+++ script/courses.js
@@ -1,24 +1,5 @@
 document.getElementById('current-year').textContent = new Date().getFullYear();
 
-const buttons = document.querySelectorAll('.filter-btn');
-const cards = document.querySelectorAll('.course-card');
-
-buttons.forEach(btn => {
-    btn.addEventListener('click', () => {
-        buttons.forEach(b => b.classList.remove('btn-primary'));
-        btn.classList.add('btn-primary');
-
-        const cat = btn.getAttribute('data-cat');
-        cards.forEach(card => {
-            if (cat === 'all' || card.dataset.category === cat) {
-                card.style.display = 'flex';
-            } else {
-                card.style.display = 'none';
-            }
-        });
-    });
-});
-
 document.addEventListener('DOMContentLoaded', function () {
     const toggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('header nav');
@@ -48,4 +29,65 @@ document.addEventListener('DOMContentLoaded', function () {
             btn.parentElement.classList.toggle('open');
         });
     });
+});
+
+// Fetching Categories
+document.addEventListener('DOMContentLoaded', function () {
+    const loader = document.getElementById('loading');
+    const coursesContainer = document.querySelector('.container.courses')
+    coursesContainer.style.display = 'none'
+    loader.style.display = 'block';
+
+    fetch('http://localhost:3131/categories')
+        .then(res => res.json())
+        .then(categories => {
+            for (let i = 0; i < categories.length; i++) {
+                const category = categories[i]
+                const categoryDiv = document.createElement('div');
+                categoryDiv.classList.add('category')
+
+                fetch(`http://localhost:3131/courses/by-category-id/${category._id}`)
+                    .then(res => res.json())
+                    .then(courses => {
+                        var content = `
+                            <button class="category-header">
+                                <span>${category.name}</span>
+                                <span class="arrow">▶</span>
+                            </button>
+                        `
+                        courses.forEach(course => {
+                            const phone = '972593617179';
+                            const message = encodeURIComponent(`مرحباً، أود التسجيل في دورة ${course.title}!`);
+                            const url = `https://wa.me/${phone}?text=${message}`;
+
+                            content += `
+                                <div class="category-content">
+                                    <div class="course-card">
+                                        <img src="images/${category.imageRef}" alt="${course.title}">
+                                        <h3>${course.title}</h3>
+                                        <p>${course.description}</p>
+                                        <a href="${url}" target="_blank" class="btn-primary">
+                                            تواصل معنا على واتساب
+                                        </a>
+                                    </div>
+                                </div>
+                            `
+                        })
+
+                        categoryDiv.innerHTML = content
+                        coursesContainer.appendChild(categoryDiv)
+
+                        if (i === categories.length - 1) {
+                            coursesContainer.style.display = 'block'
+                            loader.style.display = 'none';
+
+                            document.querySelectorAll('.category-header').forEach(btn => {
+                                btn.addEventListener('click', () => {
+                                    btn.parentElement.classList.toggle('open');
+                                });
+                            });
+                        }
+                    })
+            }
+        })
 });
